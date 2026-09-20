2baf085a3a77375f4ac5ee43572d718cdfd2e3c2
Subrahmanya <codewithsubrahmanya@gmail.com>
2026-09-20 22:09:49 +0530

    done

--- Logicss/src/sort/BubbleSort.java
+++ Logicss/src/sort/BubbleSort.java
@@ -13,7 +13,7 @@ public class BubbleSort {
 
 		for (int i = 0; i < arr.length - 1; i++) {
 			for (int j = 0; j < arr.length - 1 - i; j++) {
-				System.out.println("Comparing :" + arr[j] + " and " + arr[j + 1]);
+				System.out.println("Comparing : " + arr[j] + " and " + arr[j + 1]);
 				if (arr[j] > arr[j + 1]) {
 
 					int temp = arr[j];
